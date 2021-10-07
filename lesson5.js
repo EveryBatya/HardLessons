8be0bd7d4613ca40099942e4387a50a6d2439eb0
EveryBatya <34242079+EveryBatya@users.noreply.github.com>
2021-10-08 00:03:39 +0500

    хард дз 5 урока

---
+++ lesson5.js
@@ -0,0 +1,6 @@
+let arr = [32342, 234222, 763403, 2363, 4154246, 95470, 47674555];
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i].toString[0] == 2 || arr[i].toString[0] == 4) console.log(arr[i]);
+}
+
