555a48789afb50d1c819c4e12a35894cd0472cf9
EveryBatya <34242079+EveryBatya@users.noreply.github.com>
2021-10-08 00:04:25 +0500

    хард дз 5 урока

--- lesson5.js
+++ lesson5.js
@@ -1,6 +1,13 @@
-let arr = [32342, 234222, 763403, 2363, 4154246, 95470, 47674555];
+let arr = [832342, 234222, 763403, 2363, 4154246, 95470, 47674555];
 
 for (let i = 0; i < arr.length; i++) {
-    if (arr[i].toString[0] == 2 || arr[i].toString[0] == 4) console.log(arr[i]);
+    if (String(arr[i])[0] == 2 || String(arr[i])[0] == 4) console.log(arr[i]);
 }
 
+for (let i = 1; i < 100; i++) {
+    if (i == 2 || i == 3 || i == 5 || i == 7) console.log(i, 'Делители этого числа - 1 и ' + i);
+    else if ((i != 1) && (i % 2 != 0 && i % 3 != 0 && i % 5 != 0 && i % 7 != 0)) {
+         console.log(i, 'Делители этого числа - 1 и ' + i);
+     }
+     
+}
